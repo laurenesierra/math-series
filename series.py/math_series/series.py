421606c0685174fb90f8252901aed5176bd494ac
Lauren Sierra <laurenesierra@gmail.com>
2021-06-13 20:49:32 -0700

    finished series.py

--- series.py/math_series/series.py
+++ series.py/math_series/series.py
@@ -27,23 +27,35 @@ def fibonacci(n):
 def lucas(n):
 
   """
- The first few Lucas numbers are
+  this function has one parameter (n).
+  this function should return the nth value
+  in the fibonacci series.
+  the function is implemented using recursion.
 
- 2, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123, ....
+  The Lucas Numbers are a related series of integers that start with the values 2 and 1 rather than 0 and 1.
+  The first few Lucas numbers are:
+
+  2, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123, ....
+  """
+  if n == 0:
+    return 2
+  elif n == 1:
+    return 1
+  else:
+    return lucas(n - 1) + lucas(n - 2)
+
+def sum_series(n, a = 0, b = 1):
 
   """
+  Both the fibonacci series and the lucas numbers are based on an identical formula. Add a third function called sum_series with one required parameter and two optional parameters. The required parameter will determine which element in the series to print. The two optional parameters will have default values of 0 and 1 and will determine the first two values for the series to be produced.
 
-  a = 2
-  b = 1
-
-  if(n == 0):
+  """
+  if n == 0:
     return a
-
-  for i in range(2, n + 1):
-    c = a + b
-    a = b
-    b = c
-
+  elif n == 1:
     return b
+  else:
+    return sum_series(n - 1, a, b) + sum_series(n - 2, a, b)
+      
 
 
